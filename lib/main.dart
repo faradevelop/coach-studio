98ebea3ba5a6c0b7b433a6cba928586cc7e93c8f
fara <fatemehrasoulifar@gmail.com>
2026-07-19 14:57:28 +0330

    chore: configure Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -9,8 +16,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(color: Colors.lightBlueAccent,),
-    );
+    return MaterialApp(home: Container(color: Colors.lightBlueAccent));
   }
 }
