001f62052036e8220bc49f92c58fd266d706d5a9
fara <fatemehrasoulifar@gmail.com>
2026-07-31 18:03:49 +0330

    ref: remove unused codes

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xff2563EB);
-
-  static const background = Color(0xffF8FAFC);
-
-  static const surface = Colors.white;
-
-  static const textPrimary = Color(0xff111827);
-
-  static const textSecondary = Color(0xff6B7280);
-
-  static const error = Color(0xffDC2626);
-
   static const Color orange = Color(0xFFFF6B35);
   static const Color cream = Color(0xFFFFF8F0);
   static const Color dirtyCream = Color.fromARGB(255, 233, 222, 212);
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,18 +1,5 @@
-import 'package:coach_studio/core/theme/app_colors.dart';
-import 'package:coach_studio/core/theme/app_radius.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData light = ThemeData(
-    scaffoldBackgroundColor: AppColors.background,
-
-    colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
-
-    cardTheme: CardThemeData(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(AppRadius.md),
-      ),
-    ),
-  );
+  static ThemeData light = ThemeData(fontFamily: 'Vazirmatn');
 }
