4bb253f2a6f641fbb8a8b0c4e5015548f65a1f83
fara <fatemehrasoulifar@gmail.com>
2026-08-24 16:34:03 +0330

    refactor: add label getter for program enums

--- lib/features/workout_programs/domain/enums/program_goal.dart
+++ lib/features/workout_programs/domain/enums/program_goal.dart
@@ -1,1 +1,17 @@
-enum ProgramGoal { hypertrophy, strength, fatLoss, endurance, rehabilitation }
+enum ProgramGoal {
+  hypertrophy,
+  strength,
+  fatLoss,
+  endurance,
+  rehabilitation;
+
+  String get label {
+    return switch (this) {
+      ProgramGoal.hypertrophy => 'هایپرتروفی',
+      ProgramGoal.strength => 'قدرتی',
+      ProgramGoal.fatLoss => 'چربی‌سوزی',
+      ProgramGoal.endurance => 'استقامتی',
+      ProgramGoal.rehabilitation => 'توان‌بخشی',
+    };
+  }
+}
--- lib/features/workout_programs/domain/enums/program_level.dart
+++ lib/features/workout_programs/domain/enums/program_level.dart
@@ -1,1 +1,13 @@
-enum ProgramLevel { beginner, intermediate, advanced }
+enum ProgramLevel {
+  beginner,
+  intermediate,
+  advanced;
+
+  String get label {
+    return switch (this) {
+      ProgramLevel.beginner => 'مبتدی',
+      ProgramLevel.intermediate => 'متوسط',
+      ProgramLevel.advanced => 'پیشرفته',
+    };
+  }
+}
